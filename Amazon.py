0948270df87ef8182ccdfd60dc68203aa625f751
Tao <72544580+Stream314@users.noreply.github.com>
2025-08-28 15:09:36 +0800

    Add files via upload

---
+++ Amazon.py
@@ -0,0 +1,10 @@
+import requests
+url = ""
+try:
+    kv = {'user-agent':'Mozilla/5.0'}
+    r = requests.get(url,headers=kv)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    print(r.text[1000:2000])
+except:
+    print("error")
