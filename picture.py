5277119398697da986419e2a2a57daae99115c91
Tao <72544580+Stream314@users.noreply.github.com>
2025-08-28 15:17:32 +0800

    Add files via upload

---
+++ picture.py
@@ -0,0 +1,18 @@
+import requests
+import os
+url = ""
+root = ""
+path = root + url.split('/')[-1]
+try:
+    if not os.path.exists(root):
+        os.mkdir(root)
+    if not os.path.exists(path):
+        r = requests.get(url)
+        with open(path,'wb') as f:
+            f.write(r.content)
+            f.close()
+            print("saved")
+    else:
+        print("exist")
+except:
+    print("fail")
